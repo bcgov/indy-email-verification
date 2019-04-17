040b16f676305e0ca32863dd0ba60825693a0553
Wade Barnes <wade.barnes@shaw.ca>
2019-04-17 09:51:05 -0700

    Add initial configurations

---
+++ openshift/settings.sh
@@ -0,0 +1,13 @@
+export PROJECT_NAMESPACE="devex-von-image"
+export GIT_URI="https://github.com/nrempel/indy-email-verification.git"
+export GIT_REF="master"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Override environments, since there is only one AT THE MOMENT:
+# devex-von-image-tools
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export TOOLS="devex-von-image-tools"
+export DEPLOYMENT_ENV_NAME="tools"
+export DEV="tools"
+export TEST="tools"
+export PROD="tools"
