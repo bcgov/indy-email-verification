03220afc911bef04877aa729a000ee7b58d5f4d6
Nicholas Rempel <nbrempel@gmail.com>
2019-07-08 10:48:26 -0700

    Listen for active connection instead of response
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/email_verification/views.py
+++ src/email_verification/views.py
@@ -111,7 +111,7 @@ def webhooks(request, topic):
     logger.info(f"webhook recieved - topic: {topic} body: {request.body}")
 
     # Handle new invites, send cred offer
-    if topic == "connections" and message["state"] == "response":
+    if topic == "connections" and message["state"] == "active":
         credential_definition_id = cache.get("credential_definition_id")
         assert credential_definition_id is not None
 
