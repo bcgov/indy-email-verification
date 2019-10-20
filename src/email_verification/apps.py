864d3e02f7a04a007fd1a01d80258453185ff409
Nicholas Rempel <nbrempel@gmail.com>
2019-10-20 15:39:44 -0700

    bump schema version
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/email_verification/apps.py
+++ src/email_verification/apps.py
@@ -26,7 +26,7 @@ class EmailVerificationConfig(AppConfig):
         if cache.get("credential_definition_id") is None:
             schema_body = {
                 "schema_name": "verified-email",
-                "schema_version": "1.2.1",
+                "schema_version": "1.2.2",
                 "attributes": ["email", "time"],
             }
             schema_response = requests.post(f"{AGENT_URL}/schemas", json=schema_body)
