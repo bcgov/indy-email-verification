baa2bb8d40df9b60e3f1cbf1178b978bf5da8f59
Emiliano Suñé <emiliano.sune@gmail.com>
2020-06-19 16:25:21 -0700

    Switch to using Sovrin StagingNet
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/email-verification-agent-deploy.overrides.sh
+++ openshift/email-verification-agent-deploy.overrides.sh
@@ -1,68 +1,22 @@
-# ===================================================================================
-# Special Deployment Parameters needed for the email-verification-service instance.
-# -----------------------------------------------------------------------------------
-# ===================================================================================
-printStatusMsg(){
-  (
-    _msg=${1}
-    _yellow='\033[1;33m'
-    _nc='\033[0m' # No Color
-    printf "\n${_yellow}${_msg}\n${_nc}" >&2
-  )
-}
+_includeFile=$(type -p overrides.inc)
+if [ ! -z ${_includeFile} ]; then
+  . ${_includeFile}
+else
+  _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
+fi
 
-readParameter(){
-  (
-    _msg=${1}
-    _paramName=${2}
-    _defaultValue=${3}
-    _encode=${4}
+# ================================================================================================================
+# Special deployment parameters needed for injecting a user supplied settings into the deployment configuration
+# ----------------------------------------------------------------------------------------------------------------
 
-    _yellow='\033[1;33m'
-    _nc='\033[0m' # No Color
-    _message=$(echo -e "\n${_yellow}${_msg}\n${_nc}")
-
-    read -r -p $"${_message}" ${_paramName}
-
-    writeParameter "${_paramName}" "${_defaultValue}" "${_encode}"
-  )
-}
-
-writeParameter(){
-  (
-    _paramName=${1}
-    _defaultValue=${2}
-    _encode=${3}
-
-    if [ -z "${_encode}" ]; then
-      echo "${_paramName}=${!_paramName:-${_defaultValue}}" >> ${_overrideParamFile}
-    else
-      # The key/value pair must be contained on a single line
-      _encodedValue=$(echo -n "${!_paramName:-${_defaultValue}}"|base64 -w 0)
-      echo "${_paramName}=${_encodedValue}" >> ${_overrideParamFile}
-    fi
-  )
-}
-
-initialize(){
-  # Define the name of the override param file.
-  _scriptName=$(basename ${0%.*})
-  export _overrideParamFile=${_scriptName}.param
-
-  printStatusMsg "Initializing ${_scriptName} ..."
-
-  # Remove any previous version of the file ...
-  if [ -f ${_overrideParamFile} ]; then
-    printStatusMsg "Removing previous copy of ${_overrideParamFile} ..."
-    rm -f ${_overrideParamFile}
-  fi
-}
-
-initialize
-
-# Get the webhook URL
-readParameter "INDY_WALLET_SEED - Please provide the indy wallet seed to use with the agent:" INDY_WALLET_SEED "000000000000000000000000Steward1" 
+if createOperation; then
+  # Ask the user to supply the sensitive parameters ...
+  readParameter "WALLET_SEED - Please provide the indy wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" WALLET_SEED $(generateSeed) "false"
+else
+  # Secrets are removed from the configurations during update operations ...
+  printStatusMsg "Update operation detected ...\nSkipping the prompts for WALLET_SEED secret... \n"
+  writeParameter "WALLET_SEED" "prompt_skipped" "false"
+fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
 echo ${SPECIALDEPLOYPARMS}
-
