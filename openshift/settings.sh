6979d172998ee83cee1b859081aed2fe8d76ef68
wadeking98 <wkingnumber2@gmail.com>
2021-01-08 15:25:00 -0800

    added external nsp to build
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,13 +1,13 @@
-export PROJECT_NAMESPACE="devex-von-image"
-export GIT_URI="https://github.com/bcgov/indy-email-verification.git"
-export GIT_REF="master"
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE}
+export GIT_URI=${GIT_URI:-"https://github.com/bcgov/indy-email-verification.git"}
+export GIT_REF=${GIT_REF:-"master"}
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Override environments, since there is only one AT THE MOMENT:
 # devex-von-image-tools
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-export TOOLS="devex-von-image-tools"
-export DEPLOYMENT_ENV_NAME="tools"
-export DEV="tools"
-export TEST="tools"
-export PROD="tools"
+export TOOLS="4a9599-tools"
+export DEPLOYMENT_ENV_NAME="dev"
+export DEV="dev"
+export TEST="test"
+export PROD="prod"
