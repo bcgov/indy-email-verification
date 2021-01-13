0b011f6438bf681471ea4ba567425c30abd2dff4
wadeking98 <wkingnumber2@gmail.com>
2021-01-12 16:05:43 -0800

    got initial project running on OCP4
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,13 +1,3 @@
-export PROJECT_NAMESPACE=${PROJECT_NAMESPACE}
+export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-4a9599}
 export GIT_URI=${GIT_URI:-"https://github.com/bcgov/indy-email-verification.git"}
 export GIT_REF=${GIT_REF:-"master"}
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Override environments, since there is only one AT THE MOMENT:
-# devex-von-image-tools
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-export TOOLS="4a9599-tools"
-export DEPLOYMENT_ENV_NAME="dev"
-export DEV="dev"
-export TEST="test"
-export PROD="prod"
