51de3fa746e00654fe33b7a1af440da3e9e5adf3
wadeking98 <wkingnumber2@gmail.com>
2021-01-13 13:53:31 -0800

    got rid of demo, got email verification working
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,3 @@
 export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-4a9599}
-export GIT_URI=${GIT_URI:-"https://github.com/bcgov/indy-email-verification.git"}
+export GIT_URI=${GIT_URI:-"https://github.com/wadeking98/indy-email-verification.git"}
 export GIT_REF=${GIT_REF:-"master"}
