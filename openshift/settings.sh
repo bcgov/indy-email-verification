fc023a8d88cf7933657d0a1371637aa140ffb839
wadeking98 <wkingnumber2@gmail.com>
2021-01-21 09:54:36 -0800

    added skip_git_overrides to settigns.sh
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,3 +1,7 @@
 export PROJECT_NAMESPACE=${PROJECT_NAMESPACE:-4a9599}
 export GIT_URI=${GIT_URI:-"https://github.com/bcgov/indy-email-verification.git"}
 export GIT_REF=${GIT_REF:-"master"}
+# The templates that should not have their GIT referances(uri and ref) over-ridden
+# Templates NOT in this list will have they GIT referances over-ridden
+# with the values of GIT_URI and GIT_REF
+export skip_git_overrides="visual-verifier-deploy.yaml"
